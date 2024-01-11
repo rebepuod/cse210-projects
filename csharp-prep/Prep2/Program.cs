d972add56aae58c1b42361b2174a9c7605c7169b
rebepuod <rebecapuodziunas98@gmail.com>
2024-01-11 22:46:03 +0100

    3 core requirement

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -7,29 +7,32 @@ class Program
         Console.Write("What is the grade percentage? ");
         string userinput =Console.ReadLine();
         int grade = int.Parse(userinput);
+        string letter = "";
 
         if (grade >= 90)
         {
-            Console.WriteLine("A");
+            letter = "A";
         }
 
         else if (grade >= 80)
         {
-            Console.WriteLine("B");
+            letter = "B";
         }
 
         else if (grade >= 70)
         {
-            Console.WriteLine("C");
+            letter = "C";
         }
         else if (grade >= 60)
         {
-            Console.WriteLine("D");
+            letter = "D";
         }
         else if (grade < 60)
         {
-            Console.WriteLine("F");
+            letter = "F";
         }
+
+        Console.WriteLine(letter);
 
         if (grade >= 70)
         {
