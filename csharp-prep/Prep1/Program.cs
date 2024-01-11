f7b51a9b7b171db9e9090a0cb94d2e68c71c2da1
rebepuod <rebecapuodziunas98@gmail.com>
2024-01-11 20:56:40 +0100

    added a space

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name?");
+        Console.Write("What is your first name? ");
         string name = Console.ReadLine();
-        Console.Write("What is your last name?");
+        Console.Write("What is your last name? ");
         string last = Console.ReadLine();
 
         Console.WriteLine($"Your name is {last}, {name} {last}.");
