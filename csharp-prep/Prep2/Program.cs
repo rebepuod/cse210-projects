16c8b341616020dcdaa65905bd3dd0f7ca996568
rebepuod <rebecapuodziunas98@gmail.com>
2024-01-11 22:22:37 +0100

    First and second core Requirement

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the grade percentage? ");
+        string userinput =Console.ReadLine();
+        int grade = int.Parse(userinput);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("A");
+        }
+
+        else if (grade >= 80)
+        {
+            Console.WriteLine("B");
+        }
+
+        else if (grade >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else if (grade < 60)
+        {
+            Console.WriteLine("F");
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations!, You passed the class");
+        }
+        else 
+        {
+            Console.WriteLine("You did not passed the class");
+        }
     }
 }
